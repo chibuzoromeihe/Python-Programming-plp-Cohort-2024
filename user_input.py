961a9165c4efc77c9a14fdc1b728a481a2031ccd
Chibuzor Omeihe <corneliusomeihe@gmail.com>
2024-03-14 11:55:39 +0100

    week 1 assignment

--- user_input.py
+++ user_input.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-# user_input.py
 
 name = input("Enter your name: ")
 
@@ -9,16 +7,3 @@ location = input("Enter your location: ")
 
 print("Hello {}, you are {} years old and live in {} ".format(name, age, location))
 
-=======
-# user_input.py
-
-name = input("Enter your name: ")
-
-age = input("Enter your age: ")
-
-location = input("Enter your location: ")
-
-print("Hello {}, you are {} years old and live in {} ".format(name, age, location))
-
-
->>>>>>> 35a9a25e22a979efaedf6d6ca9b4639ac0c93359
